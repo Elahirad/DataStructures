79a18247e1d5fe4468df2f8f183993a1005bc773
Ali Elahiraad <alielahirad8087@gmail.com>
2021-12-10 20:24:11 +0330

    Adding final binary tree codes.

--- Trees/BinaryTrees.cs
+++ Trees/BinaryTrees.cs
@@ -118,8 +118,20 @@ namespace Trees
         }
         private int Min(Node root)
         {
+            if (root == null) return 0;
             if (root.LeftChild == null && root.RightChild == null) return root.Value;
             return Math.Min(root.Value, Math.Min(Min(root.LeftChild), Min(root.RightChild)));
+        }
+
+        public int Max()
+        {
+            return Max(_root);
+        }
+        private int Max(Node root)
+        {
+            if (root == null) return 0;
+            if (root.LeftChild == null && root.RightChild == null) return root.Value;
+            return Math.Max(root.Value, Math.Max(Max(root.LeftChild), Max(root.RightChild)));
         }
 
         public bool Equals(BinaryTree second)
@@ -193,6 +205,69 @@ namespace Trees
             if (distance == 0) arr.Add(root.Value);
             GetNodesAtDistance(root.LeftChild, distance - 1, arr);
             GetNodesAtDistance(root.RightChild, distance - 1, arr);
+        }
+
+        public int Size()
+        {
+            return Size(_root);
+        }
+
+        private int Size(Node root)
+        {
+            if (root == null) return 0;
+            if (root.LeftChild == null && root.RightChild == null) return 1;
+            return Size(root.LeftChild) + Size(root.RightChild) + 1;
+        }
+
+        public int CountLeaves()
+        {
+            return GetNodesAtDistance(Height()).Count;
+        }
+
+        public bool Contains(int to_search)
+        {
+            return Contains(_root, to_search);
+        }
+        private bool Contains(Node root, int to_search)
+        {
+            if (root == null) return false;
+            if (root.Value == to_search) return true;
+            return Contains(root.LeftChild, to_search) || Contains(root.RightChild, to_search);
+        }
+
+        public bool AreSibling(int first, int second)
+        {
+            return AreSibling(_root, first, second);
+        }
+        private bool AreSibling(Node root, int first, int second)
+        {
+            if (root == null || root.LeftChild == null || root.RightChild == null) return false;
+            if ((root.LeftChild.Value == first && root.RightChild.Value == second)
+                || (root.LeftChild.Value == second && root.RightChild.Value == first))
+                return true;
+            return AreSibling(root.LeftChild, first, second) || AreSibling(root.RightChild, first, second);
+        }
+
+        public List<int> GetAncestors(int child) {
+            var arr = new List<int>();
+            GetAncestors(_root, child, arr);
+            return arr;
+        }
+        private bool GetAncestors(Node root, int child, List<int> arr)
+        {
+            if (root == null) return false;
+            if (root.Value == child) return true;
+            if (GetAncestors(root.LeftChild, child, arr))
+            {
+                arr.Add(root.Value);
+                return true;
+            }
+            if (GetAncestors(root.RightChild, child, arr))
+            {
+                arr.Add(root.Value);
+                return true;
+            }
+            return false;
         }
     }
 }
