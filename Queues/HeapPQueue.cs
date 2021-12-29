1a457ee2d7612322ca39efd47f2fe16bdbcdcd4b
NYXTheShadow <alielahirad8087@gmail.com>
2021-12-30 00:03:42 +0330

    Adding Min Heap Priority Queues.

--- Queues/HeapPQueue.cs
+++ Queues/HeapPQueue.cs
@@ -1,6 +1,36 @@
 using Trees;
 namespace Queues
 {
+
+    public class MinHeapPQueue
+    {
+        private MinHeap _heap;
+
+        public MinHeapPQueue(int capacity)
+        {
+            _heap = new MinHeap(capacity);
+        }
+
+        public void Enqueue(int item)
+        {
+            _heap.Insert(item);
+        }
+
+        public int Dequeue()
+        {
+            return _heap.Remove();
+        }
+
+        public bool IsFull()
+        {
+            return _heap.IsFull();
+        }
+
+        public bool IsEmpty()
+        {
+            return _heap.IsEmpty();
+        }
+    }
     public class MaxHeapPQueue
     {
         private MaxHeap _heap;
