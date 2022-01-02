e505a5e16691d655ebe3a46bbb9f97ce11bbc8e1
NYXTheShadow <alielahirad8087@gmail.com>
2022-01-02 15:56:47 +0330

    Removing ArrayTrie and Adding Trie implemented with HashTable, Adding some methods

--- Trees/Tries.cs
+++ Trees/Tries.cs
@@ -1,42 +1,97 @@
 using System.Collections;
 namespace Trees
 {
-    public class ArrayTrie
+    public class Trie
     {
         private class TrieNode
         {
-            private char _value;
+            public char Value { get; set; }
+            private Dictionary<char, TrieNode> _dict;
+            public bool IsEndOfWord { get; set; }
 
-            public TrieNode[] Children;
-
-            public bool IsEnd;
             public TrieNode(char value)
             {
-                _value = value;
-                Children = new TrieNode[26];
-                IsEnd = false;
+                this.Value = value;
+                this._dict = new Dictionary<char, TrieNode>();
+                this.IsEndOfWord = false;
+            }
+
+            public bool HasAnyChild()
+            {
+                return this._dict.Keys.ToList<char>().Count > 0;
+            }
+            public bool HasChild(char key)
+            {
+                if (this._dict.ContainsKey(key)) return true;
+                return false;
+            }
+
+            public TrieNode GetChild(char key)
+            {
+                if (HasChild(key)) return this._dict[key];
+                return null;
+            }
+
+            public void SetChild(char key, TrieNode value)
+            {
+                this._dict[key] = value;
+            }
+
+            public void RemoveChild(char key)
+            {
+                this._dict.Remove(key);
             }
         }
 
         private TrieNode _root;
 
-        public ArrayTrie()
+        public Trie()
         {
             _root = new TrieNode(' ');
         }
 
-        public void Insert(string text)
+        public void Insert(string item)
         {
-            var root = _root;
-            foreach (var ch in text)
+            var buffer = _root;
+            foreach (var ch in item)
             {
-                if (root.Children[ch - 97] == null)
-                {
-                    root.Children[ch - 97] = new TrieNode(ch);
-                }
-                root = root.Children[ch - 97];
+                if (!buffer.HasChild(ch)) buffer.SetChild(ch, new TrieNode(ch));
+
+                buffer = buffer.GetChild(ch);
             }
-            root.IsEnd = true;
+            buffer.IsEndOfWord = true;
+        }
+
+        public bool Contains(string item)
+        {
+            if (item == null) return false;
+            var buffer = _root;
+            foreach (var ch in item)
+            {
+                if (buffer.HasChild(ch))
+                    buffer = buffer.GetChild(ch);
+                else return false;
+            }
+            return buffer.IsEndOfWord;
+        }
+
+        public void Remove(string item)
+        {
+            if (!Contains(item)) return;
+            Remove(_root, item, 0);
+        }
+
+        private void Remove(TrieNode root, string item, int index)
+        {
+            if (index == item.Length)
+            {
+                root.IsEndOfWord = false;
+                return;
+            }
+            var child = root.GetChild(item[index]);
+            Remove(child, item, index + 1);
+            if (!child.HasAnyChild() && !child.IsEndOfWord)
+                root.RemoveChild(child.Value);
         }
     }
 }
